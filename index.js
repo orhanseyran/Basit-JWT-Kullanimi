0b0e9827ce2bb500f8200a24825bc5a0f6791474
orhanseyran <orhanseyran35@gmail.com>
2024-08-28 16:40:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var giris = (name , password ) => {
+    
+   var name =  document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (document.getElementById("email").value == "") {
+        alert("Email Alanı Boş Bırakılamaz");
+        
+    }
+    if (document.getElementById("password").value == "") {
+        alert("Şifre Alanı Boş Bırakılamaz");
+
+    }
+    if(name == "orhanseyran35@gmail.com" && password == 142536)
+    {
+        window.location.href = "panel.html";
+        localStorage.setItem("giris", "true");
+    }
+
+}
